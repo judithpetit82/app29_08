e2a9528b8d6cd626a696379ff5e00d24f0b7588d
Samdjm <sam.djm93@gmail.com>
2022-08-30 12:11:53 +0200

    Fin Login

--- Components/Container/Login/Login.jsx
+++ Components/Container/Login/Login.jsx
@@ -4,19 +4,29 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 export default function Login() {
   //les variables d'états:
   const [emailInput, setEmailInput] = useState("");
+  const [emailError, setEmailError] = useState("");
+
   const [passwordInput, setPasswordInput] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   //Les fonctions handle:
   function handleEmail(text) {
     setEmailInput(text);
+    setEmailError("");
   }
   function handlePassword(text) {
     setPasswordInput(text);
+    setPasswordError("");
   }
 
   //Fonction de validation
   function login() {
-    console.log(emailInput + ", connexion réussie!");
+    if (emailInput.includes("@") && passwordInput.length >= 6) {
+      console.log(emailInput + ", connexion réussie!");
+    } else {
+      setEmailError(!emailInput.includes("@") ? "Email invalide!" : "");
+      setPasswordError(passwordInput.length < 6 ? "Mot de passe trop court!" : "");
+    }
   }
 
   return (
@@ -26,12 +36,13 @@ export default function Login() {
         keyboardType='email-address'
         onChangeText={handleEmail}
       />
-
+      <Text>{emailError}</Text>
       <TextInput
         placeholder='Mot de passe'
         secureTextEntry
         onChangeText={handlePassword}
       />
+      <Text>{passwordError}</Text>
       <TouchableOpacity onPress={login}>
         <Text>Se connecter</Text>
       </TouchableOpacity>
