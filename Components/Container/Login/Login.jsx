180ade425c1332de3bb40ccdd30003cb7f3344f3
Samdjm <sam.djm93@gmail.com>
2022-08-30 11:51:36 +0200

    Inputs dans Login

--- Components/Container/Login/Login.jsx
+++ Components/Container/Login/Login.jsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 export default function Login() {
+  //les variables d'états:
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  //Les fonctions handle:
+  function handleEmail(text) {
+    setEmailInput(text);
+  }
+  function handlePassword(text) {
+    setPasswordInput(text);
+  }
+
+  //Fonction de validation
+  function login() {
+    console.log(emailInput + ", connexion réussie!");
+  }
+
   return (
-    <View>
-      <Text>Se connecter</Text>
+    <View accessibilityRole='form'>
+      <TextInput
+        placeholder='Email'
+        keyboardType='email-address'
+        onChangeText={handleEmail}
+      />
+
+      <TextInput
+        placeholder='Mot de passe'
+        secureTextEntry
+        onChangeText={handlePassword}
+      />
+      <TouchableOpacity onPress={login}>
+        <Text>Se connecter</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- Components/Pages/Auth/Auth.jsx
+++ Components/Pages/Auth/Auth.jsx
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Login from "../../Container/Login/Login";
+import Signup from "../../Container/Signup/Signup";
 import Card from "../../HOC/Card/Card";
 
 export default function Auth() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  function toggleLogin() {
+    setIsLogin(!isLogin);
+  }
+  //Exercice:
+  //Utiliser la variable d'état isLogin pour afficher un texte different:
+  //Pas encore membre, inscrivez vous! -> isLogin = true
+  //Deja membre? Connectez vous! -> isLogin = false
+
   return (
     <View style={styles.container}>
-      <Card title='Bienvenue!' content='Veuillez vous authentifier!'>
-        <Text style={{ color: "red" }}>Un composant potentiel</Text>
-        <Text>Un autre composant potentiel</Text>
+      <Card
+        title='Bienvenue!'
+        content={isLogin ? "Veuillez vous connecter!" : "Veuillez vous inscrire!"}
+      >
+        {isLogin ? <Login /> : <Signup />}
+
+        <TouchableOpacity>
+          <Text onClick={toggleLogin}>
+            {isLogin
+              ? "Pas encore membre, inscrivez vous !"
+              : "Déjà membre ? Connectez-vous !"}
+          </Text>
+        </TouchableOpacity>
       </Card>
     </View>
   );
